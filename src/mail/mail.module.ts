c4bfcb11e3a6d0be1d1a45dbd79445ae96192d4b
GEDDY90 <geddydossou@gmail.com>
2024-07-08 10:49:33 +0100

    mail

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,20 +1,20 @@
 import { DynamicModule, Module } from '@nestjs/common';
-import { CONFIG_OPTIONS } from 'src/common/common.constants';
+import { CONFIG_OPTIONS } from '../common/common.constants';
 import { MailModuleOptions } from './mail.interfaces';
 import { MailService } from './mail.service';
 
 @Module({})
 export class MailModule {
-    static forRoot(options: MailModuleOptions) : DynamicModule {
+    static forRoot(options: MailModuleOptions): DynamicModule {
         return {
             module: MailModule,
             providers: [
                 {
-                provide: CONFIG_OPTIONS,
-                useValue: options,
-            },
-            MailService
-        ],
+                    provide: CONFIG_OPTIONS,
+                    useValue: options,
+                },
+                MailService,
+            ],
             exports: [MailService],
         };
     }
